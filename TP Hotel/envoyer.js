5f9825a8060e13a87dbd375761ecf60f9e55cc75
HAMZA <hamabbas910@gmail.com>
2023-05-10 00:21:47 +0200

    Welcome!

---
+++ TP Hotel/envoyer.js
@@ -0,0 +1,5 @@
+document.getElementById('name1').innerHTML=localStorage.getItem('passvalue');
+document.getElementById('reservation1').innerHTML=localStorage.getItem('date1value');
+document.getElementById('sortiedefinitive').innerHTML=localStorage.getItem('date2value');
+document.getElementById('perso').innerHTML=localStorage.getItem('personnesvalue');
+document.getElementById('chrm').innerHTML=localStorage.getItem('chambrevalue');
